2aad448900c39d34b9e9835cd17cb8c6d1be0a8f
Danylo Lebediev <danilleock@gmail.com>
2018-06-28 10:44:17 +0200

    Fixed bug with Shoulders matchers config.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,3 +67,14 @@ RSpec.configure do |config|
     end
   end
  end
+
+ Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :active_record
+    with.library :active_model
+    with.library :action_controller
+    with.library :rails
+  end
+end
+end
